508aa0e1c9958cbd02ef8bd47d224780a261239f
stephen talley <stvlley.amzn@gmail.com>
2022-02-01 19:27:37 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+
+function mondayWork(activity = "go to the office") {
+    return ( `This Monday, I will ${activity}.`)
+
+}
+
+function wrapAdjective(flare = "*") {
+    
+    return function (body = "a hard worker"){
+    return `You are ${flare}${body}${flare}!`
+    }
+}
+console.log(wrapAdjective("||")("a dedicated programmer"))
